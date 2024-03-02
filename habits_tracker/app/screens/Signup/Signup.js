4de83a5d74a2841075f0af97f63156df63b8bd22
mwawrzyniak <mwawrzyniak@pop-os.localdomain>
2024-03-02 14:53:18 +0100

    feat(auth): implements register page with styles + add js file to import all screens

--- habits_tracker/app/screens/Signup/Signup.js
+++ habits_tracker/app/screens/Signup/Signup.js
@@ -1,13 +1,89 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { styles } from "./styles";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import SocialMediaButton from "../../components/Buttons/SocialMediaButton";
+import Images from "../../utils/constants/images";
+import styles from "./styles";
 
-export default function Signup() {
+/**
+ * Cette page permet à l'utilisateur de s'inscrire
+ * @param {Object} navigation
+ * @returns {JSX.Element} Page d'inscription
+ */
+export default function Signup({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.background}>
+        <View style={styles.overlay} />
+        <Image
+          style={styles.logo}
+          source={Images.Logo}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Nom"
+          placeholderTextColor="#aaaaaa"
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="E-mail"
+          placeholderTextColor="#aaaaaa"
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#aaaaaa"
+          secureTextEntry
+          placeholder="Mot de passe"
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#aaaaaa"
+          secureTextEntry
+          placeholder="Confirme Mot de passe"
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => console.log("Register")}
+        >
+          <Text style={styles.buttonTitle}>Créer son compte</Text>
+        </TouchableOpacity>
+        <View style={styles.footerView}>
+          <Text style={styles.footerText}>
+            As-tu deja un compte ?{" "}
+            <Text
+              onPress={() => console.log("Login")}
+              style={styles.footerLink}
+            >
+              Se connecter
+            </Text>
+          </Text>
+        </View>
+        <View style={styles.divider}>
+          <View style={styles.dividerLine} />
+          <Text style={styles.dividerText}>OU</Text>
+          <View style={styles.dividerLine} />
+        </View>
+        <View style={styles.socialMedia}>
+          <SocialMediaButton
+            source={Images.GoogleIcon}
+            onPress={() => console.log("Google")}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
--- habits_tracker/app/screens/Signup/styles.js
+++ habits_tracker/app/screens/Signup/styles.js
@@ -1,10 +1,82 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({
+export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    alignItems: "center",
+    backgroundColor: "#404040",
+  },
+  background: {
+    flex: 1,
+    width: "90%"
+  },
+  title: {},
+  logo: {
+    height: 150,
+    width: 150,
+    alignSelf: "center",
+    margin: 20,
+  },
+  input: {
+    height: 48,
+    borderRadius: 5,
+    overflow: "hidden",
+    backgroundColor: "white",
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingLeft: 16,
+    fontSize: 18,
+  },
+  button: {
+    backgroundColor: "#F1A44A",
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 20,
+    height: 48,
+    borderRadius: 5,
     alignItems: "center",
     justifyContent: "center",
+  },
+  buttonTitle: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  footerView: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+  footerText: {
+    fontSize: 16,
+    color: "white",
+  },
+  footerLink: {
+    color: "#F2A94A",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  divider: {
+    flexDirection: "row",
+    marginVertical: 40,
+  },
+  dividerLine: {
+    flex: 1,
+    height: 1,
+    backgroundColor: "white",
+  },
+  dividerText: {
+    marginHorizontal: 20,
+    fontSize: 18,
+    color: "white"
+  },
+  socialMedia: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  socialMediaIcon: {
+    width: 40,
+    height: 40,
   },
 });
