663839e9f35d8591a3c7af41538b9bf6cd465b2d
mwawrzyniak <mwawrzyniak@pop-os.localdomain>
2024-03-02 13:22:12 +0100

    feat(auth): init signin and signup screens + init styles files

---
+++ habits_tracker/app/screens/Signup/Signup.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { styles } from "./styles";
+import { Text, View } from "react-native";
+
+export default function Signup() {
+  return (
+    <View style={styles.container}>
+      <Text>Login</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
