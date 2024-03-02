82317cd906747c4c3b523e7f27f4a9415b26b263
Lienub <lienub.68@gmail.com>
2024-03-02 13:20:52 +0100

    fix(splashscreen): added on platform support

--- habits_tracker/App.js
+++ habits_tracker/App.js
@@ -1,3 +1,5 @@
+// Native imports
+import { Platform } from 'react-native';
 // Navigation imports
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -8,11 +10,23 @@ import MainTest from './app/screens/MainTest';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+// Check if the platform is not web
+  if (Platform.OS !== 'web') {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="SplashScreen" headerMode="none">
+          <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }}/>
+          <Stack.Screen name="MainTest" component={MainTest} options={{ headerShown: false, animation: 'none' }}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
+  // Render only main screen on web
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SplashScreen">
-        <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="MainTest" component={MainTest} options={{ headerShown: false, animation: 'none' }} />
+      <Stack.Navigator initialRouteName="MainTest" headerMode="none">
+        <Stack.Screen name="MainTest" component={MainTest} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
