304f527866f2fdb6db74b63a88b362d7df8390ea
mwawrzyniak <mwawrzyniak@pop-os.localdomain>
2024-03-02 16:35:19 +0100

    fix(server): manage errors for register api

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,7 +1,7 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { validationResult } = require('express-validator');
-const Users = require('../models/users');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
+const Users = require("../models/users");
 
 const register = async (req, res) => {
   try {
@@ -11,18 +11,22 @@ const register = async (req, res) => {
     }
 
     const { username, email, password, googleId } = req.body;
-
-    // Vérifier si l'utilisateur s'inscrit avec Google
+    if(!username) {
+      return res.status(400).json({status: "username_failed", message: "Username est requis !"})
+    } else if(!email) {
+      return res.status(400).json({status: "email_failed", message: "Email est requis !"})
+    } else if(!password) {
+      return res.status(400).json({status: "password_failed", message: "Password est requis !"})
+    }
     if (googleId) {
       const existingUser = await Users.findOne({ where: { googleId } });
       if (existingUser) {
-        return res.status(400).json({ message: 'Google ID already registered' });
+        return res.status(400).json({status: "failed", message: "Votre compte existe deja !"})
       }
     } else {
-      // Si l'utilisateur s'inscrit avec e-mail et mot de passe
       const existingUser = await Users.findOne({ where: { email } });
       if (existingUser) {
-        return res.status(400).json({ message: 'Email already registered' });
+        return res.status(400).json({status: "email_failed", message: "Email a deja été utilisé !"})
       }
     }
 
@@ -32,15 +36,20 @@ const register = async (req, res) => {
       username,
       email,
       password: hashedPassword,
-      googleId
+      googleId,
     });
 
     const token = jwt.sign({ userId: newUser.id }, process.env.JWT_SECRET);
+    const userInfo = {
+      id: newUser.id,
+      username: newUser.username,
+      email: newUser.email,
+    };
 
-    res.status(201).json({ token });
+    return res.json({ status: "success", token, user: userInfo });
+
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -56,17 +65,17 @@ const login = async (req, res) => {
     if (googleId) {
       user = await Users.findOne({ where: { googleId } });
       if (!user) {
-        return res.status(401).json({ message: 'Invalid Google ID' });
+        return res.status(401).json({ message: "Invalid Google ID" });
       }
     } else {
       user = await Users.findOne({ where: { email } });
       if (!user) {
-        return res.status(401).json({ message: 'Invalid email or password' });
+        return res.status(401).json({ message: "Invalid email or password" });
       }
 
       const passwordMatch = await bcrypt.compare(password, user.password);
       if (!passwordMatch) {
-        return res.status(401).json({ message: 'Invalid email or password' });
+        return res.status(401).json({ message: "Invalid email or password" });
       }
     }
 
@@ -74,40 +83,40 @@ const login = async (req, res) => {
 
     res.status(200).json({ token });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
 const getUserByGoogleId = async (googleId) => {
   const user = await Users.findOne({ where: { googleId } });
   return user;
-}
+};
 
 const resetPassword = async (req, res) => {
   try {
     const { token, newPassword } = req.body;
 
     if (!token || !newPassword) {
-      return res.status(400).json({ message: 'Token de réinitialisation et nouveau mot de passe requis' });
+      return res.status(400).json({
+        message: "Token de réinitialisation et nouveau mot de passe requis",
+      });
     }
 
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
     const user = await Users.findByPk(decodedToken.userId);
     if (!user) {
-      return res.status(404).json({ message: 'Utilisateur non trouvé' });
+      return res.status(404).json({ message: "Utilisateur non trouvé" });
     }
 
     const hashedPassword = await bcrypt.hash(newPassword, 10);
 
-
     await user.update({ password: hashedPassword });
 
-    res.status(200).json({ message: 'Mot de passe réinitialisé avec succès' });
+    res.status(200).json({ message: "Mot de passe réinitialisé avec succès" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
