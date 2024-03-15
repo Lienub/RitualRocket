ef3f909c3c65d5402df8c5de936b6d39d9882f68
mwawrzyniak <mwawrzyniak@pop-os.localdomain>
2024-03-15 12:17:13 +0100

    chore(app): modify main navigation

--- habits_tracker/App.js
+++ habits_tracker/App.js
@@ -1,21 +1,67 @@
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import AuthNavigation from "./app/navigation/AuthNavigation";
+import { authUserExists } from "./app/services/users";
+import SignupScreen from "./app/screens/Signup/SignupScreen";
+import SigninScreen from "./app/screens/Signin/SigninScreen";
+import HomeScreen from "./app/screens/Home/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [userExists, setUserExists] = useState(false);
+
+  useEffect(() => {
+    checkUserExists();
+  }, []);
+
+  const checkUserExists = async () => {
+    try {
+      const exists = await authUserExists();
+      setUserExists(exists);
+    } catch (error) {
+      console.error("Error checking user existence:", error.message);
+    }
+  };
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Signup">
-        <Stack.Screen
-          name="AuthNavigation"
-          component={AuthNavigation}
-          options={{
-            headerShown: false,
-          }}
-        />
+      <Stack.Navigator>
+        {!userExists ? (
+          <>
+            <Stack.Screen
+              name="Signin"
+              component={SigninScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Signup"
+              component={SignupScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+                gestureEnabled: false,
+              }}
+            />
+          </>
+        ) : (
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              headerShown: false,
+              gestureEnabled: false,
+            }}
+          />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
