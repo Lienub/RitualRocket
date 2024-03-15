be204dc244fa62eef6caa77562d43d7ce6c5c79d
mwawrzyniak <mwawrzyniak@pop-os.localdomain>
2024-03-15 17:41:34 +0100

    chore(homescreen): retrieve user information in storage + display username for example

--- habits_tracker/app/screens/Home/HomeScreen.js
+++ habits_tracker/app/screens/Home/HomeScreen.js
@@ -1,5 +1,28 @@
 import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import { getUserInfo } from "../../services/users";
+import styles from "./styles";
 
 export default function HomeScreen({ navigation }) {
-  return <></>;
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const userInfo = await getUserInfo();
+        setUser(userInfo);
+      } catch (error) {
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {
+        (user) ? <Text style={{marginTop: 100, fontSize: 30}}>Welcome {user.username}</Text> : <Text>Loading...</Text>
+      }
+    </View>
+  );
 }
