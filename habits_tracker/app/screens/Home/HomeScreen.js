6cd1ecafd6856819fd4dfa16231ed72c15ffade2
mwawrzyniak <mwawrzyniak@pop-os.localdomain>
2024-03-29 12:37:53 +0100

    feat: add calendar agenda for home screen to display tasks

--- habits_tracker/app/screens/Home/HomeScreen.js
+++ habits_tracker/app/screens/Home/HomeScreen.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Alert, StyleSheet } from "react-native";
+import CalendarStrip from "react-native-calendar-strip";
 import { getUserInfo } from "../../services/users";
 import styles from "./styles";
 
 export default function HomeScreen({ navigation }) {
   const [user, setUser] = useState({});
+  const [selectedDate, setSelectedDate] = useState(
+    new Date().toISOString().split("T")[0]
+  );
 
   useEffect(() => {
     const fetchUserInfo = async () => {
@@ -12,17 +16,44 @@ export default function HomeScreen({ navigation }) {
         const userInfo = await getUserInfo();
         setUser(userInfo);
       } catch (error) {
+        console.error("Error fetching user info:", error);
       }
     };
 
     fetchUserInfo();
   }, []);
 
+  useEffect(() => {
+    console.log(selectedDate);
+  }, [selectedDate, setSelectedDate]);
+
   return (
     <View style={styles.container}>
-      {
-        (user) ? <Text style={{marginTop: 100, fontSize: 30}}>Welcome {user.username}</Text> : <Text>Loading...</Text>
-      }
+      <View style={styles.block}>
+        <CalendarStrip
+          scrollable={true}
+          scrollerPaging={true}
+          calendarAnimation={{ type: "sequence", duration: 30 }}
+          daySelectionAnimation={{
+            type: "border",
+            borderWidth: 2,
+            borderHighlightColor: "white",
+            duration: 300,
+          }}
+          style={styles.calendarStrip}
+          calendarHeaderStyle={{ color: "white", fontSize: 20 }}
+          dateNumberStyle={{ color: "white" }}
+          dateNameStyle={{ color: "white" }}
+          highlightDateNumberStyle={{ color: "white" }}
+          highlightDateNameStyle={{ color: "white" }}
+          selectedDate={selectedDate}
+          onDateSelected={(date) => {
+            setSelectedDate(date.toISOString().split("T")[0]);
+          }}
+        />
+        <Text style={{ color: "white" }}>Welcome, {user.username}</Text>
+        <Text style={{ color: "white" }}>Selected date: {selectedDate}</Text>
+      </View>
     </View>
   );
 }
--- habits_tracker/app/screens/Home/styles.js
+++ habits_tracker/app/screens/Home/styles.js
@@ -3,7 +3,17 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     backgroundColor: "#404040",
+    paddingHorizontal: 20,
+  },
+  block: {
+    height: 300,
+    width: "100%",
+    flex:1,
+  },
+  calendarStrip: {
+    height: 100,
+    marginTop: 80,
+    width: "100%",
   },
 });
