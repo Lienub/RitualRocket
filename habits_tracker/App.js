8dc54ce4031c9fbb62d0ca34271ed7d1467a253c
Lienub <lienub.68@gmail.com>
2024-03-31 19:00:12 +0200

    fix(navigation): fixed splashscreen behavior and added custom navbar

--- habits_tracker/App.js
+++ habits_tracker/App.js
@@ -3,31 +3,56 @@ import { Platform } from 'react-native';
 // Navigation imports
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {BottomTabBar} from './app/components/BottomTabBar';
 // Component / Screen imports
 import SplashScreen from './app/components/SplashScreen';
 import MainTest from './app/screens/MainTest';
+// React imports
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-// Check if the platform is not web
-  if (Platform.OS !== 'web') {
-    return (
-      <NavigationContainer>
-        <Tab.Navigator tabBar={props => <BottomTabBar {...props} state={{...props.state, routes: props.state.routes.slice(1,1)}} initialRouteName="Splash" headerMode="none"></BottomTabBar> }>
-          <Tab.Screen name="Splash" component={SplashScreen} options={{ headerShown: false, tabBarStyle: { display: "none" }, tabBarVisible: false }}/>
-          <Tab.Screen name="MainTest" component={MainTest} options={{ headerShown: false, animation: 'none' }}/>
-        </Tab.Navigator>
-      </NavigationContainer>
-    );
+  const [assetsLoaded, setAssetsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Loading of assets
+    const loadAssets = async () => {
+      // Simulating asset loading for 3 seconds (the duration of the animation)
+      await new Promise(resolve => setTimeout(resolve, 3800));
+
+      /* 
+        PUT HERE THE ASSETS THAT HAVE TO BE LOADED
+      */
+
+      setAssetsLoaded(true);
+    };
+
+    loadAssets();
+  }, []);
+
+
+  if (!assetsLoaded && Platform.OS !== 'web') {
+    return <SplashScreen assetsLoaded={assetsLoaded} />;
   }
 
-  // Render only main screen on web
   return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="MainTest" headerMode="none">
-        <Tab.Screen name="MainTest" component={MainTest} options={{ headerShown: false }}/>
-      </Tab.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Tab.Navigator 
+          screenOptions={{
+                  tabBarStyle: {
+                    borderTopWidth: 0,
+                    elevation: 0,
+                  },
+          }}
+          tabBar={props => <BottomTabBar {...props} />}
+        >
+          <Tab.Screen name="Home" component={MainTest} />
+          <Tab.Screen name="Calendar" component={MainTest} />
+          <Tab.Screen name="Completed" component={MainTest} />
+          <Tab.Screen name="Profile" component={MainTest} />
+        </Tab.Navigator>
+      </NavigationContainer>
   );
 }
