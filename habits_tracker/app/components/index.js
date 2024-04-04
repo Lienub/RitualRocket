3a47d86db734c0859257349a00d141e2237b91a8
Lienub <lienub.68@gmail.com>
2024-04-04 20:54:38 +0200

    feat(design): created custom components

---
+++ habits_tracker/app/components/index.js
@@ -0,0 +1,5 @@
+import { NiceModal } from "./NiceModal";
+import { NiceTextInput } from "./NiceTextInput";
+import { StyleContainer } from "./StyleContainer";
+
+export { NiceModal, NiceTextInput, StyleContainer };
