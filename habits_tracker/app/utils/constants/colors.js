7f6760e0bd8362dfbb16475ed5cfcafd6538a27c
Lienub <lienub.68@gmail.com>
2024-04-04 20:56:24 +0200

    style(design): color stylesheet

---
+++ habits_tracker/app/utils/constants/colors.js
@@ -0,0 +1,16 @@
+export const COLORS = {
+    light: {
+        primary: "#F2994A",
+        secondary: "#C78F5E",
+        tertiary: "#f2f2f2",
+        text: "#000",
+        background: "#fff",
+    },
+    dark: {
+        primary: "#F2994A",
+        secondary: "#C78F5E",
+        tertiary: "#1a1a1a",
+        text: "#fff",
+        background: "#303030",
+    },
+}
